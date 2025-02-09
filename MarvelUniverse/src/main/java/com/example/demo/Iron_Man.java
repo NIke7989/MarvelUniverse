62b3b277be7c00cb44c0252f291cd3a16b2bfa66
NIke7989 <nike7989k@gmail.com>
2025-02-09 16:20:13 -0600

    winter solider  added

--- MarvelUniverse/src/main/java/com/example/demo/Iron_Man.java
+++ MarvelUniverse/src/main/java/com/example/demo/Iron_Man.java
@@ -20,7 +20,8 @@ public class Iron_Man {
 	        powerLevels.put("Scarlet Witch", 97);
 	        powerLevels.put("Thanos", 99);
 	        powerLevels.put("Hawkeye", 20);
-	        powerLevels.put("Red Skull", 25);
+	        powerLevels.put("Red Skull", 50);
+	        powerLevels.put("Winter Solider", 60);
 	    }
 
 	    public static String determineWinner(String hero1, String hero2) {
